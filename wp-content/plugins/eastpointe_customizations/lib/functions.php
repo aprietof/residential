6eab2d17383a065154be372e697ffeeef929f9d3
Adrian Prieto <adrianprieto00@gmail.com>
2018-03-30 16:54:28 -0400

    Add admin styles sheet for property details admin section

--- wp-content/plugins/eastpointe_customizations/lib/functions.php
+++ wp-content/plugins/eastpointe_customizations/lib/functions.php
@@ -1,2 +1,6 @@
 <?php 
 
+function eapt_load_admin_style() {
+  wp_enqueue_style( 'eapt_admin_css', plugin_dir_url( __DIR__ ) . 'assets/css/admin.css' );
+}
+add_action( 'admin_head', 'eapt_load_admin_style' );
